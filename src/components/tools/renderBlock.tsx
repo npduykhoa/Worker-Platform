b8b60e7569b96e5e32a60e489f93bbb4cb82589a
npduykhoa <npduykhoa@gmail.com>
2024-10-11 08:30:27 +0700

    Config eslint, prettier & tailwindcss

--- src/components/tools/renderBlock.tsx
+++ src/components/tools/renderBlock.tsx
@@ -1,32 +1,31 @@
-import React from 'react'
+import React from 'react';
 
 export interface RenderBlockSectionDataProps {
-  component: React.FunctionComponent
-  componentName: string
-  props?: object
-  elementType?: keyof JSX.IntrinsicElements
-  id?: string
-  className?: string
+	component: React.FunctionComponent;
+	componentName: string;
+	props?: object;
+	elementType?: keyof JSX.IntrinsicElements;
+	id?: string;
+	className?: string;
 }
 
 export interface RenderBlockProps {
-  sectionData: RenderBlockSectionDataProps
-  key: number | string
+	sectionData: RenderBlockSectionDataProps;
+	key: number | string;
 }
 
 /**
  * Render element by name, type and props
  */
 export const renderBlock = (sectionData: RenderBlockProps['sectionData'], key: RenderBlockProps['key']) => {
-  const { component, componentName, elementType = 'section', id, props: componentData, className } = sectionData
-  const componentKey = `${componentName}-${key}`
-  const WrapperBlock = elementType
+	const { component, componentName, props: componentData, className } = sectionData;
+	const componentKey = `${componentName}-${key}`;
 
-  // const sectionId = formatString({ string: componentName })
+	// const sectionId = formatString({ string: componentName })
 
-  return (
-    <section className={`w-full ${className}`} key={componentKey} data-component-name={componentName}>
-      {React.createElement(component, { ...componentData })}
-    </section>
-  )
-}
+	return (
+		<section className={`w-full ${className}`} key={componentKey} data-component-name={componentName}>
+			{React.createElement(component, { ...componentData })}
+		</section>
+	);
+};
